076bdf39a2b32f97ee9a8a7928c511fd247b1768
Hardik Katehara <hardikkatehara@gmail.com>
2021-05-24 14:21:52 +0530

    changes

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -4,14 +4,19 @@ import subprocess
 
 # import python_utils
 
-# list of non-strictly typed files
-not_strict_typed_files = [
-    'core/controllers/base.py',
-    'core/controllers/admin.py',
-    'core/controllers/base_test.py',
-    'core/controllers/admin_test.py',
+# list of non-strictly typed files and directories
+not_strict_typed_files_and_directories = [
+    'core/',
     'scripts',
-    'third_party'
+    'third_party',
+    'extensions/',
+    'jobs/',
+    'core/controllers/admin.py',
+]
+
+# list of typed files
+strict_typed_files = [
+    'core/controllers/admin.py',
 ]
 
 CONFIG_FILE_PATH = os.path.join('.','mypy-strict.ini')
@@ -35,13 +40,13 @@ def main(args=None):
 
     if unused_parsed_args.files:
         process = subprocess.Popen(
-            ['mypy', '--config-file', CONFIG_FILE_PATH] + unused_parsed_args.files,
+            ['mypy', '--namespace-packages', '--explicit-package-bases', '--config-file', CONFIG_FILE_PATH] + unused_parsed_args.files,
             stdin=subprocess.PIPE)
 
     # take files from config-file
     else:
-        cmd = ['mypy', '--exclude', '|'.join(not_strict_typed_files),
-        '--config-file', CONFIG_FILE_PATH, '.']
+        cmd = ['mypy', '--namespace-packages', '--explicit-package-bases', '--exclude', '|'.join(not_strict_typed_files_and_directories),
+        '--config-file', CONFIG_FILE_PATH, '.'] + strict_typed_files
 
         process = subprocess.Popen(cmd, stdin=subprocess.PIPE)
 
